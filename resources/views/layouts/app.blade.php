a56d315b87974eb933619fa0b8fb5057c551e9e5
mikhail-karim <hellcatcelicakarim@gmail.com>
2025-07-21 08:14:57 +0700

    e

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,10 +107,10 @@
         </div>
     </div>
 
-    {{-- Bootstrap 4 JS --}}
+    {{-- Bootstrap 4 JS
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> --}}
 
     @livewireScripts
 </body>
